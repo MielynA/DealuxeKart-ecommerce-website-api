3dc57a6f6b2ff139d1c934e10c91d02ee0de9417
Mielyn Acosta <mielynacosta@pursuit.org>
2019-03-20 12:00:07 -0400

    added routes and services for Orderline

---
+++ backend/routes/orderline.js
@@ -0,0 +1,53 @@
+const app = require('express').Router();
+const orderLineService = require ('../services/orders_services'); 
+
+//--- CREATE ORDERS 
+app.post('/', (req,res) => {
+    const {orderId,productId,quantity} = req.body;
+   orderLineService.post(orderId,productId,quantity).then(()=>{
+       res.json({msg: 'new order line is created!'})
+   })
+   .catch(err=>{
+       res.status(404).json({error: err.toString('utf-8')})
+   })
+})
+
+//--- READ/GET ORDERS 
+app.get('/:orderline_id', (req,res)=>{
+    const { orderline_id } = req.params; 
+    if(!orderline_id) {
+       res.json({msg: `order ${orderline_id} does not exiist` })
+    }
+    orderLineService.get(orderline_id).then((data)=>{
+        res.json({msg: data})
+    })
+    .catch(err=>{
+        res.status(404).json({error: err.toString('utf-8')})
+    })
+})
+
+//--- UPDATE ORDERS 
+app.put('/:orderline_id', (req,res) => {
+    const { orderline_id } = req.params;  
+    const {orderId,productId,quantity} = req.body;
+   orderLineService.put(order_id, orderId,productId,quantity).then(()=>{
+       res.json({msg: `${orderline_id} is udpated!`})
+   })
+   .catch(err=>{
+       res.status(404).json({error: err.toString('utf-8')})
+   })
+})
+
+//--- DELETE ORDERS 
+app.delete('/:orderline_id', (req,res)=>{
+    const{orderline_id} = req.params; 
+    const {orderId,productId,quantity} = req.body;
+        orderLineService.delete(orderline_id,orderId,productId,quantity).then(()=>{
+            res.json({msg: `${orderline_id} has been deleted`})
+        })
+
+    .catch(err=>{
+        res.status(404).json({error: err.toString('utf-8')})
+    })
+})
+module.exports = {orderLineRoutes: app, } 
---
+++ backend/services/orderline_service.js
@@ -0,0 +1,24 @@
+const db = require('../db/database');
+orderLineService = {}
+
+
+orderLineService.post = (orderId,productId,quantity) => {
+    return db.none('INSERT INTO OrderLine (orderId,productId,quantity) VALUES (${orderId},${productId},${quantity})', 
+         {orderId,productId,quantity})
+    }
+
+orderLineService.get = (orderId) => {
+    return db.one('SELECT * FROM OrderLine WHERE orderId = ${orderId}', {orderId})
+}
+
+orderLineService.put = (orderId,productId,quantity) => {
+    return db.none('UPDATE OrderLine SET orderId = ${orderId},productId = ${productId},quantity = ${quantity}', 
+     {orderId,productId,quantity})
+}
+
+orderLineService.delete = (orderId) => {
+    return db.none('DELETE FROM OrderLine WHERE orderId = ${orderId}', {orderId})
+}
+
+
+module.exports = orderLineService;
