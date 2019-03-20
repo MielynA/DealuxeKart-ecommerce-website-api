bf318482137d5c9cf2ced03336807340907365fc
Mielyn Acosta <mielynacosta@pursuit.org>
2019-03-20 11:22:12 -0400

    added service and routes for Orders

---
+++ backend/routes/orders.js
@@ -0,0 +1,53 @@
+const app = require('express').Router();
+const orderService = require ('../services/orders_services'); 
+
+//--- CREATE ORDERS 
+app.post('/', (req,res) => {
+    const {productId, customerId, orderDate, orderNum, price, total, color, orderStatus, paymentType} = req.body;
+   orderService.post(productId, customerId, orderDate, orderNum, price, total, color, orderStatus, paymentType).then(()=>{
+       res.json({msg: 'new orders is created!'})
+   })
+   .catch(err=>{
+       res.status(404).json({error: err.toString('utf-8')})
+   })
+})
+
+//--- READ/GET ORDERS 
+app.get('/:order_id', (req,res)=>{
+    const { order_id } = req.params; 
+    if(!order_id) {
+       res.json({msg: `order ${order_id} does not exiist` })
+    }
+    orderService.get(order_id).then((data)=>{
+        res.json({msg: data})
+    })
+    .catch(err=>{
+        res.status(404).json({error: err.toString('utf-8')})
+    })
+})
+
+//--- UPDATE ORDERS 
+app.put('/:order_id', (req,res) => {
+    const { order_id } = req.params;  
+    const {productId, customerId, orderDate, orderNum, price, total, color, orderStatus, paymentType} = req.body;
+   orderService.put(order_id, productId, customerId, orderDate, orderNum, price, total, color, orderStatus, paymentType).then(()=>{
+       res.json({msg: `customer ${order_id} is udpated!`})
+   })
+   .catch(err=>{
+       res.status(404).json({error: err.toString('utf-8')})
+   })
+})
+
+//--- DELETE ORDERS 
+app.delete('/:order_id', (req,res)=>{
+    const{order_id} = req.params; 
+    const {productId, customerId, orderDate, orderNum, price, total, color, orderStatus, paymentType} = req.body;
+        orderService.delete(order_id,productId, customerId, orderDate, orderNum, price, total, color, orderStatus, paymentType).then(()=>{
+            res.json({msg: `customer ${order_id} has been deleted`})
+        })
+
+    .catch(err=>{
+        res.status(404).json({error: err.toString('utf-8')})
+    })
+})
+module.exports = {orderRoutes: app, } 
--- backend/services/orders_services.js
+++ backend/services/orders_services.js
@@ -8,16 +8,16 @@ orderService.post = (productId, customerId, orderDate, orderNum, price, total, c
 }
 
 orderService.get = (orderId) => {
-    return db.one('SELECT * FROM Customer WHERE orderId = ${orderId}', {orderId})
+    return db.one('SELECT * FROM Orders WHERE orderId = ${orderId}', {orderId})
 }
 
 orderService.put = (productId, customerId, orderDate, orderNum, price, total, color, orderStatus, paymentType) => {
-    return db.none('UPDATE Customer SET productId = ${productId}, customerId = ${customerId}, orderDate = ${orderDate}, orderNum = ${orderNum}, price = ${price}, total = ${total}, color = ${color}, orderStatuse = ${orderStatus}, paymentType = ${paymentType} WHERE orderId = ${orderId}', 
+    return db.none('UPDATE Orders SET productId = ${productId}, customerId = ${customerId}, orderDate = ${orderDate}, orderNum = ${orderNum}, price = ${price}, total = ${total}, color = ${color}, orderStatuse = ${orderStatus}, paymentType = ${paymentType} WHERE orderId = ${orderId}', 
      {productId, customerId, orderDate, orderNum, price, total, color, orderStatus, paymentType})
 }
 
 orderService.delete = (orderId) => {
-    return db.none('DELETE FROM Customer WHERE orderId = ${orderId}', {orderId})
+    return db.none('DELETE FROM Orders WHERE orderId = ${orderId}', {orderId})
 }
 
 
