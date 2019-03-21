39c0637036d606cbbd86897940a333d79acd6225
Mielyn Acosta <mielynacosta@pursuit.org>
2019-03-21 13:00:35 -0400

    fixed unit test failure

---
+++ tests/customer.test.js
@@ -0,0 +1,78 @@
+jest.mock('pg-promise');
+const pgp = require('pg-promise');
+
+pgp.mockImplementation(() => {
+    return function() {
+        return {
+            none: () => Promise.resolve(),
+            one: () => Promise.resolve(),
+            
+        }
+    }
+})
+const customerService = require ('../backend/services/customer_services');
+//--- TEST FOR POST CUSTOMER SERVICE 
+test("objectWithoutInfo", ()=>{
+   
+customerService.post("Mie", 123, "mie@email.com", "Queens", "NYC", "NYC", 59679).then(data => {
+        expect(data).toBe(undefined)
+        //expect(data).toEqual({userName : "Mie", userPassword : 10383, email : "lara@email.com", billingAdd : "Brooklyn", city: "NYC", state: "NYC", creditCard: 01010 })
+    })
+})
+
+test("objectWithInfo", ()=>{
+   
+    customerService.post("Mie", 123, "mie@email.com", "Queens", "NYC", "NYC", 59679).then(data => {
+           // expect(data).toBe(data)
+            expect(data).toEqual({userName : "Mie", userPassword : 123, email : "mie@email.com", billingAdd : "Queens", city: "NYC", state: "NYC", creditCard: 59679 })
+        })
+    })
+
+//--- TEST FOR GET CUSTOMER SERVICE
+test("objectWithoutInfo", ()=>{
+
+customerService.get("Mie", 123, "mie@email.com", "Queens", "NYC", "NYC", 59679).then(data => {
+        expect(data).toBe(undefined)
+        //expect(data).toEqual({userName : "Mie", userPassword : 10383, email : "lara@email.com", billingAdd : "Brooklyn", city: "NYC", state: "NYC", creditCard: 01010 })
+    })
+})
+
+test("objectWithInfo", ()=>{
+   
+    customerService.get("Mie", 123, "mie@email.com", "Queens", "NYC", "NYC", 59679).then(data => {
+           // expect(data).toBe(data)
+            expect(data).toEqual({userName : "Mie", userPassword : 123, email : "mie@email.com", billingAdd : "Queens", city: "NYC", state: "NYC", creditCard: 59679 })
+        })
+    })
+//--- TEST FOR UPDATE/PUT CUSTOMER SERVICE 
+test("objectWithoutInfo", ()=>{
+    
+    customerService.put("Mie", 123, "mie@email.com", "Queens", "NYC", "NYC", 59679).then(data => {
+            expect(data).toBe(undefined)
+            //expect(data).toEqual({userName : "Mie", userPassword : 10383, email : "lara@email.com", billingAdd : "Brooklyn", city: "NYC", state: "NYC", creditCard: 01010 })
+        })
+    })
+    
+test("objectWithInfo", ()=>{
+ 
+    customerService.put("Mie", 123, "mie@email.com", "Queens", "NYC", "NYC", 59679).then(data => {
+               // expect(data).toBe(data)
+                expect(data).toEqual({userName : "Mie", userPassword : 123, email : "mie@email.com", billingAdd : "Queens", city: "NYC", state: "NYC", creditCard: 59679 })
+            })
+        })
+
+//--- TEST FOR DELETE CUSTOMER SERVICE
+test("objectWithoutInfo", ()=>{
+
+    customerService.delete("Mie", 123, "mie@email.com", "Queens", "NYC", "NYC", 59679).then(data => {
+            expect(data).toBe(undefined)
+            //expect(data).toEqual({userName : "Mie", userPassword : 10383, email : "lara@email.com", billingAdd : "Brooklyn", city: "NYC", state: "NYC", creditCard: 01010 })
+        })
+    })
+test("objectWithInfo", ()=>{
+       
+        customerService.delete("Mie", 123, "mie@email.com", "Queens", "NYC", "NYC", 59679).then(data => {
+               // expect(data).toBe(data)
+                expect(data).toEqual({userName : "Mie", userPassword : 123, email : "mie@email.com", billingAdd : "Queens", city: "NYC", state: "NYC", creditCard: 59679 })
+            })
+        })
