e6e8c6d87d7419b007abed558fb4ef5f5b57ccbc
Mielyn Acosta <mielynacosta@pursuit.org>
2019-03-26 12:16:10 -0400

    added firebase, firebase auth to protect the router, added cors etc

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const {app, } = require('./backend/app');
 
 //--- RUNNING ENVI SERVER PORT 
-app.listen(process.env.PORT || 3002);
-console.log('port:', process.env.PORT || 3002 ,'is listening..');
+app.listen(process.env.PORT || 5001);
+console.log('port:', process.env.PORT || 5001 ,'is listening..');
